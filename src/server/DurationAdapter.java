d47df3e86a20f4360409f4fbcd98b82b0051f475
BlackSeno <dedseno449@gmail.com>
2025-05-03 12:20:49 +0300

    9ый спринт. 2ая попытка (исправления для Git 2)

--- src/server/DurationAdapter.java
+++ src/server/DurationAdapter.java
@@ -5,6 +5,7 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 import java.time.Duration;
+
 public class DurationAdapter extends TypeAdapter<Duration> {
     @Override
     public void write(JsonWriter out, Duration value) throws IOException {
